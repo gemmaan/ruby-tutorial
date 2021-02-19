4d435e0936c7ce3e6fcae1900a35abf76387c0f7
gemmaan <gemma.nur@btpn.com>
2021-02-19 21:49:15 +0700

    WIP: Show author in index

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,6 @@
 class Idea < ApplicationRecord
     mount_uploader :picture, PictureUploader
     enum status: [:Needs_Approval, :Approved] 
+    belongs_to :author
     #enum status: { active: 1, inactive: 0}
 end
